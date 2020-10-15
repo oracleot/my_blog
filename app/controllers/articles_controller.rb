07249fa56b07f39b34a01518e541aa50a0dc8b6b
Damilola Oduronbi <doduronbi@gmail.com>
2020-10-15 22:16:11 +0100

    Implement for all the CUD actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,11 +15,13 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    flash.notice = "Article '#{@article.title}' has been created!"
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id]).destroy
+    flash.notice = "Article '#{@article.title}' deleted!"
     redirect_to articles_path
   end
 
@@ -30,6 +32,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' updated!"
     redirect_to article_path(@article)
   end
 end
