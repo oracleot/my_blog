7ff7e29d066d9f1d045823567b161e2d5e656432
Damilola Oduronbi <doduronbi@gmail.com>
2020-10-15 22:05:52 +0100

    Implement edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,8 +14,6 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    # @article.title = params[:article][:title]
-    # @article.body = params[:article][:body]
     @article.save
     redirect_to article_path(@article)
   end
@@ -23,5 +21,15 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id]).destroy
     redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
   end
 end
